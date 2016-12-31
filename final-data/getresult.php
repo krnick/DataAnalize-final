3fbab9cfbf0a88fd6a08dbcd447ee3aee6982751
Your Name <you@example.com>
2016-12-31 12:23:43 +0800

    done

--- final-data/getresult.php
+++ final-data/getresult.php
@@ -31,6 +31,29 @@ $result = $conn->query($newsql);
 
 if ($result->num_rows > 0) {
     // output data of each row
+
+?>
+
+<link rel="stylesheet" href="all.css">
+
+<div class="header">
+ <div class="demo">
+    <form class="" action="getresult.php" method="post">
+      <input type="text" placeholder="搜尋" name="keyword">
+      <input type="submit" value="確認">
+    </form>
+
+  </div>
+
+
+</div>
+
+
+<div class="container">
+
+<?php
+
+
     while($row = $result->fetch_assoc()) {
 
 	echo "<a href='newkeyword/$row[filename]'  >檔案來源$row[filename]</a> <br>" ;
@@ -42,8 +65,26 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 $conn->close();
-
-
-
-
 ?>
+
+
+</div>
+
+
+<style>
+    input[type="submit"]{padding:5px 15px; background:#ccc; border:0 none;
+cursor:pointer;
+-webkit-border-radius: 5px;
+border-radius: 5px; }
+
+    input[type="text"]{padding:5px 15px; border:2px black solid;
+cursor:pointer;
+-webkit-border-radius: 5px;
+border-radius: 5px; }
+
+.demo{
+    padding-top: 50px;
+    text-align: center
+}
+</style>
+
